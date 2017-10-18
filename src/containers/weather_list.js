d9cd46618b78f03515db6a33e4289331af03b09c
d.shabin@gmail.com <d.shabin@gmail.com>
2017-10-19 02:39:36 +0400

    Add Fetch Temp

---
+++ src/containers/weather_list.js
@@ -0,0 +1,44 @@
+import React , { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+
+  renderWeather(cityData) {
+    const name = cityData.city.name
+    const temps = cityData.list.map(weather => weather.main.temp)
+    console.log(temps)
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          {temps}
+        </td>
+      </tr>
+    )
+
+  }
+
+  render(){
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temp</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return { weather : state.weather}
+}
+
+export default connect(mapStateToProps)(WeatherList)
